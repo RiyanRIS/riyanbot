509b1107567a52053325cd76c67bba91177242c6
riyanris <riyanrisky129@gmail.com>
2021-05-01 11:49:28 +0700

    add lib/tasks/schedule.php

--- lib/tasks/schedule.php
+++ lib/tasks/schedule.php
@@ -1,3 +1,21 @@
 <?php
+// Telegram function which you can call
+function telegram($msg)
+{
+  global $telegrambot, $telegramchatid;
+  $url = 'https://api.telegram.org/bot' . $telegrambot . '/sendMessage';
+  $data = array('chat_id' => $telegramchatid, 'text' => $msg);
+  $options = array('http' => array('method' => 'POST', 'header' => "Content-Type:application/x-www-form-urlencoded\r\n", 'content' => http_build_query($data),),);
+  $context = stream_context_create($options);
+  $result = file_get_contents($url, false, $context);
+  return $result;
+}
 
-header("Location: https://riyanbot.herokuapp.com/lima");
+// Set your Bot ID and Chat ID.
+$telegrambot = '1369088735:AAGkBavShqR7Lt3CFfv_QLkvr6S2n45CvBU';
+$telegramchatid = 780207093;
+
+date_default_timezone_set("Asia/Jakarta");
+$text = "Ini adalah pukul " . date("H:i") . "WIB";
+// Function call with your own text or variable
+telegram($text);
