c45a35403beb19edbee6734f28b43b20ab28104a
riyanris <riyanrisky129@gmail.com>
2021-06-16 14:34:53 +0700

    update sitik

--- app/Views/errors/html/error_404.php
+++ app/Views/errors/html/error_404.php
@@ -1,1 +1,3 @@
-<title>Server Bot Telegram 🇮🇩</title>Server Bot Telegram 🇮🇩</br></br>❤️ <a target='_blank' href='https://github.com/Eleirbag89/TelegramBotPHP'>https://github.com/Eleirbag89/TelegramBotPHP</a></br>🤖 <a target='_blank' href='https://t.me/riyantesBot'>@riyantesBot</a></br></br>Enjoy..😃
+<?php
+
+echo "<link id='favicon' rel='icon' type='image/png' href='https://i.ibb.co/BzNVj8K/logo.png'><title>Server Bot Telegram 🇮🇩</title>Server Bot Telegram 🇮🇩</br></br>❤️ <a target='_blank' href='https://github.com/Eleirbag89/TelegramBotPHP'>https://github.com/Eleirbag89/TelegramBotPHP</a></br>🤖 <a target='_blank' href='https://t.me/riyantesBot'>@riyantesBot</a></br></br>Enjoy..😃";
